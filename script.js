87598d77be7b1bd499739d7bb6e1a5846ac01ffd
Yure Rafael <112043267+yure07@users.noreply.github.com>
2023-09-17 20:26:56 -0300

    add: validation user with API

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-console.log('teste')
+const getApi = async () => {
+    const result = await fetch(`https://apigenerator.dronahq.com/api/INEAjZVi/wallet_users`)
+    const users = await result.json()
+    return users
+}
 
-const clickLogin = () => {
+const clickLogin = async () => {
     const email = document.getElementById('input-email').value
 
     if(email.lenght < 7 || !email.includes('@')) {
@@ -9,7 +13,25 @@ const clickLogin = () => {
         return
     }
 
-    localStorage.setItem('@wallet-app:email', email)
-    email.value = ''
-    window.open('./src/pages/home/index.html', '_self')
+    try {
+        const users = await getApi()
+        let userName
+        let userId
+        users.map((user) => {
+            if(user.email === email){
+                userName = user.name
+                userId = user.id
+            }
+        })
+        if(userName === undefined || userId === undefined) {
+            alert('usuario nao encontrado :(')
+            return
+        }
+        localStorage.setItem('@wallet-app:email', email)
+        localStorage.setItem('@wallet-app:email', userName)
+        localStorage.setItem('@wallet-app:email', userId)
+        window.open('./src/pages/home/index.html', '_self')
+    } catch (error) {
+        console.log({error})
+    }
 }
