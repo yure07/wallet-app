f46d84d291de2d49682766bef59d18eec1d25d21
Yure Rafael <112043267+yure07@users.noreply.github.com>
2023-09-20 15:17:02 -0300

    feat: implement cards finances

--- src/pages/home/script.js
+++ src/pages/home/script.js
@@ -1,4 +1,34 @@
-window.onload = () => {
-    const emailUser= localStorage.getItem('@wallet-app:email')
-    console.log(emailUser)
+const loadData = () => {
+    const data = new Date()
+    const formated = data.toLocaleDateString()
+    return formated
 }
+
+const loadInfo = () => {
+    const nameUser = localStorage.getItem('@wallet-app:UserName')
+    const emailUser= localStorage.getItem('@wallet-app:UserEmail')
+    
+    const divInfos = document.getElementById('infos')
+    
+    const divLetterUser = document.createElement('div')
+    const letterUser = document.createElement('h2')
+    const emailName = document.createElement('a')
+    const exitLink = document.createElement('a')
+    
+    divLetterUser.classList.add('letter-user')
+    letterUser.innerHTML = nameUser.charAt(0)
+    emailName.innerHTML = emailUser
+    exitLink.classList.add('exit-account')
+    exitLink.innerHTML = 'sair'
+    divLetterUser.appendChild(letterUser)
+    
+    divInfos.appendChild(divLetterUser)
+    divInfos.appendChild(emailName)
+    divInfos.appendChild(exitLink)
+}
+
+window.onload = () => {
+    loadInfo()
+}
+
+const data = loadData()
